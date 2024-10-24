eaff92c8380056106c69a9a220dd897220319c2f
Pedro Hurtado <pedrohurt@gmail.com>
2024-10-24 16:26:26 +0200

    calendar

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,14 @@
 import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+//import * as bootstrap from 'bootstrap'
+
+/*
+    cambiar el tema a drak
+*/
+
+//document.documentElement.setAttribute('data-bs-theme','dark')
+const root = document.querySelector(':root');
+if(root){
+    root.setAttribute('data-bs-theme','dark')
+}
